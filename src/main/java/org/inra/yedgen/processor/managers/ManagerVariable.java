deb4ad008bc622e54ddd2e9acf2bfc21e481343e
Yahiaoui Rachid <rac021@hotmail.com>
2016-11-17 10:34:26 +0100

    Update ManagerVariable.java

--- src/main/java/org/inra/yedgen/processor/managers/ManagerVariable.java
+++ src/main/java/org/inra/yedgen/processor/managers/ManagerVariable.java
@@ -227,15 +227,15 @@ public class ManagerVariable {
                          .flatMap( set -> set.stream())
                          .map( s -> s.remove(PATTERN_PARALLEL))
                          .count() ;
-             
-      // How ?VARIABLE will be updated after applying KeyValues method 
-      variable.getKeyValues().put( PATTERN_VARIABLE, variable.getVariableName()) ;
       
       managerPatternParallel.applyKeyValues( new HashSet<>(generatedGraphNodes) , 
                                                  variable.getKeyValues()  )     ;
 
+      // How ?VARIABLE will be updated after applying KeyValues method 
+      variable.getKeyValues().put( PATTERN_VARIABLE, variable.getVariableName()) ;
+        
       // Restoring original nodes for next process
-      managerNode.restoreOriginalNodes() ;
+      managerNode.restoreOriginalNodes()  ;
       
       return  generatedGraphNodes         ;
       
