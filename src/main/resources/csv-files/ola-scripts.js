17ffd18ad8b45f20539acad1ce9377d774c01a09
Yahiaoui Rachid <rac021@hotmail.com>
2016-11-19 00:50:35 +0100

    Update ola-scripts.js

--- src/main/resources/csv-files/ola-scripts.js
+++ src/main/resources/csv-files/ola-scripts.js
@@ -2,5 +2,9 @@
 function camelize(str) {
    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
      return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
-   }).replace(/\s+/g, '');
+   }).replace(/\s+/g, '').capitalizeFirstLetter() ; 
+}
+
+String.prototype.capitalizeFirstLetter = function()     {
+    return this.charAt(0).toUpperCase() + this.slice(1) ;
 }
