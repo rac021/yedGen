72c70beb72c652397082590dca64b79fdd1ce3d1
Yahiaoui Rachid <rac021@hotmail.com>
2016-11-18 17:28:06 +0100

    Create CsvProperties.java

---
+++ src/main/java/org/inra/yedgen/properties/CsvProperties.java
@@ -0,0 +1,38 @@
+
+package org.inra.yedgen.properties;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.inra.yedgen.processor.scripts.ScriptsEngine;
+
+/**
+ *
+ * @author ryahiaoui
+ */
+public class CsvProperties {
+    
+    private Configuration config  = null                               ;
+    private String        csvFile = "../data/csv-files/ola-properties" ;
+    
+    private ScriptsEngine scriptsEngine ;
+    
+     List<String> getPropety( String column ) {
+       return config.getList(column ) ;
+     }
+
+    public CsvProperties( String csvFile, String jsFile  ) {
+        
+     try {
+            this.scriptsEngine = new ScriptsEngine( jsFile )          ;
+            this.config        = new PropertiesConfiguration(csvFile) ;
+        } catch (ConfigurationException ex) {
+            Logger.getLogger( CsvProperties.class.getName() )
+                                          .log( Level.SEVERE, null, ex ) ;
+        }
+    }
+     
+}
