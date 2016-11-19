0b891edc252bbf6392e84df5f8b86eb7f0ab28c0
Yahiaoui Rachid <rac021@hotmail.com>
2016-11-19 15:34:43 +0100

    Update Main.java

--- src/main/java/entypoint/Main.java
+++ src/main/java/entypoint/Main.java
@@ -11,81 +11,79 @@ import org.inra.yedgen.processor.Processor;
  
 public class Main {
     
-       public static void main (String[] args) throws Exception    {
+  public static void main (String[] args) throws Exception    {
         
-        String directory = null , outFile = null , ext = null , csv = null ;
+    String directory = null , outFile = null , ext = null , csv = null ;
         
-        String prf = null , js = null                                      ;
+    String prf = null , js = null                                      ;
         
-        boolean includingGraphVariables = false ,  verbose = false ;
+    boolean includingGraphVariables = false ,  verbose = false ;
        
-        int     nbParams                = 0     ;
+    int     nbParams                = 0     ;
        
-        for ( int i = 0 ; i < args.length ; i++ ) {
+    for ( int i = 0 ; i < args.length ; i++ ) {
             
-            String token = args[i] ;
+       String token = args[i] ;
            
-            switch(token)   {
-                case "-d"   :  directory = args[i+1] ; nbParams += 2 ;
-                                break ;
-                case "-out" :  outFile   = args[i+1] ; nbParams += 2 ;
-                                break ;
-                case "-ext" :  ext       = args[i+1] ; nbParams += 2 ;
-                                break ;            
-                case "-csv" :  csv       = args[i+1] ; nbParams += 2 ;
-                                break ;    
-                case "-prf" :  prf       = args[i+1] ; nbParams += 2 ;
-                                break ;            
-                case "-js" :  js         = args[i+1] ; nbParams += 2 ;
-                                break ;            
-                case "-ig" :   includingGraphVariables = true ; 
-                                nbParams += 1 ;
-                                break ;            
-                case "-v"  :   verbose = true ; 
-                                nbParams += 1 ;
-                                break ;            
-            }
-        }
+       switch(token)   {
+         
+         case "-d"   :  directory = args[i+1] ; nbParams += 2 ;
+                         break ;
+         case "-out" :  outFile   = args[i+1] ; nbParams += 2 ;
+                         break ;
+         case "-ext" :  ext       = args[i+1] ; nbParams += 2 ;
+                         break ;            
+         case "-csv" :  csv       = args[i+1] ; nbParams += 2 ;
+                         break ;    
+         case "-prf" :  prf       = args[i+1] ; nbParams += 2 ;
+                         break ;            
+         case "-js" :  js         = args[i+1] ; nbParams += 2 ;
+                         break ;            
+         case "-ig" :   includingGraphVariables = true ; 
+                         nbParams += 1 ;
+                         break ;            
+         case "-v"  :   verbose = true ; 
+                         nbParams += 1 ;
+                         break ;            
+       }
+    }
        
-        if( nbParams < 7 ) {
+    if( nbParams < 7 ) {
             
-            System.out.println ( " missing parameters " ) ;
+       System.out.println ( " missing parameters " ) ;
+       return ;
+    }
+        
+    if(directory.isEmpty() || outFile.isEmpty()) {
             
-            return ;
-        }
+       System.out.println (" directory or outFile is Empty " ) ;
+       return ;
+    }
         
-        if(directory.isEmpty() || outFile.isEmpty()) {
-            
-            System.out.println (" directory or outFile is Empty " ) ;
-            
-            return ;
-        }
+    if(ext.length() == 0 ) ext = ".graphml"                      ;
         
-        if(ext.length() == 0 ) ext = ".graphml"               ;
+      long startTime = System.currentTimeMillis()                ;  
         
-        long startTime = System.currentTimeMillis()           ;  
+      Processor processor = new Processor( directory             ,
+                                           ext                   ,
+                                           toAbsolutePath(prf)   ,
+                                           toAbsolutePath(js)  
+                                          )                      ;
         
-        Processor processor = new Processor( directory             ,
-                                             ext                   ,
-                                             toAbsolutePath(prf)   ,
-                                             toAbsolutePath(js)  
-                                           )                       ;
+      processor.process( outFile, csv , includingGraphVariables, verbose ) ;
         
-        processor.process( outFile, csv , includingGraphVariables, verbose ) ;
+      long executionTime = System.currentTimeMillis() - startTime ;
         
-        long executionTime = System.currentTimeMillis() - startTime ;
+      System.out.println(" Elapsed seconds : "                    + 
+                                    executionTime / 1000 + " s" ) ; 
         
-        System.out.println(" Elapsed seconds : " + 
-                                      executionTime / 1000 + " s" ) ; 
-        
-        System.out.println(" ")                                     ;
+      System.out.println(" ")                                     ;
     }
  
-    private static String toAbsolutePath( String relativePath ) {
-        
-        if( relativePath == null || relativePath.isEmpty() ) return null ;
-        
-        Path path = Paths.get(relativePath)     ;
-        return path.toAbsolutePath().toString() ;
+ 
+    private static String toAbsolutePath( String relativePath )        {
+      if( relativePath == null || relativePath.isEmpty() ) return null ;
+      Path path = Paths.get(relativePath)                              ;
+      return path.toAbsolutePath().toString().replaceAll("/../", "/")  ;
     }
 }
