672716e69fdac1f5f6b00b27981037809304a764
Yahiaoui Rachid <rac021@hotmail.com>
2016-11-21 12:01:07 +0100

    Update Main.java

--- src/main/java/entypoint/Main.java
+++ src/main/java/entypoint/Main.java
@@ -32,7 +32,7 @@ public class Main {
         
     boolean includingGraphVariables = false ,  verbose = false ;
        
-    int     nbParams                = 0     ;
+    int     nbParams                = 0       ;
        
     for ( int i = 0 ; i < args.length ; i++ ) {
             
