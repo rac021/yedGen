e88dc7dc3d61e4c9965d975bec4ccfce919e93e4
Yahiaoui Rachid <rac021@hotmail.com>
2017-09-21 16:17:49 +0200

    Update Processor.java

--- src/main/java/org/inra/yedgen/processor/Processor.java
+++ src/main/java/org/inra/yedgen/processor/Processor.java
@@ -9,6 +9,7 @@ import java.io.IOException ;
 import java.nio.file.Files ;
 import java.nio.file.Paths ;
 import java.util.ArrayList ;
+import java.util.regex.Pattern ;
 import java.util.stream.Stream ;
 import java.util.logging.Level ;
 import java.util.logging.Logger ;
