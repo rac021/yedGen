1a285084394e4260f46620385667f37ce45077cd
Yahiaoui Rachid <rac021@hotmail.com>
2017-11-03 13:15:17 +0100

    Update Processor.java

--- src/main/java/org/inra/yedgen/processor/Processor.java
+++ src/main/java/org/inra/yedgen/processor/Processor.java
@@ -339,8 +339,8 @@ public class Processor {
 			               + extension                    ;
                      
                       if( graphExtractor.getMagicFilter() != null  &&
-			  ! graphExtractor.getMagicFilter(
-				          .trim().isEmpty())        {
+			  ! graphExtractor.getMagicFilter()
+				          .trim().isEmpty() )       {
                           
                             /* Split if MagicFilter Enabled */
                             
